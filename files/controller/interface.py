603a0bd1df2a972c4ea14937ed5582baf08590c0
viktoriussuwandi <viktorius.tori@gmail.com>
2023-06-06 15:31:57 +0000

    update class interface

--- files/controller/interface.py
+++ files/controller/interface.py
@@ -6,3 +6,10 @@ class Interface :
     self.tk.title("Watermark Desktop App")
     self.tk.minsize(width=350,height=550)
     self.tk.config(bg="#deeaee")
+    self.update_attr()
+
+  def update_attr(self) :
+    pass
+
+  def action_attr(self) :
+    pass
