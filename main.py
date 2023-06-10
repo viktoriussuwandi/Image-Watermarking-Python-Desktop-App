be283ff24d4f67787de742e089a68f3e0db4e40a
viktoriussuwandi <viktorius.tori@gmail.com>
2023-06-10 06:39:30 +0000

    create canvas and add colors

--- main.py
+++ main.py
@@ -2,22 +2,37 @@
 # from PIL import Image,ImageTk
 
 from tkinter import *
+from PIL import Image, ImageTk
 
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
 tk = Tk()
 tk.title("Watermark Desktop App")
-tk.geometry("500x600")
-# tk.minsize(width=500,height=600)
+tk.minsize(width=500,height=600)
 tk.config(bg="#fefbd8")
 
-canvas = Canvas(width=450, height=350)
+canvas_height = 350
+canvas_width  = 450
+
+canvas = Canvas(width=canvas_width, height=canvas_height)
 canvas.config(bg="#B1DDC6", highlightthickness=0)
 canvas.grid(row=0, column=0)
 canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-bg_pos = (0, 0)
-bg_front = PhotoImage(file="files/img/background.png")
-background = canvas.create_image(bg_pos, image=bg_front)
-canvas.itemconfig(background, image=bg_front)
+#-------------------------------------------------------------------------------
+# IMAGE MODIFICATION
+#-------------------------------------------------------------------------------
 
+image = Image.open("files/img/background.png")
+
+img_size = (canvas_width,canvas_height)
+img_pos = (canvas_width/2,canvas_height/2)
+
+image = image.resize(img_size)
+photo_img = ImageTk.PhotoImage(image)
+
+upload_img = canvas.create_image(img_pos, image=photo_img, anchor=CENTER)
+canvas.itemconfig(upload_img, image=photo_img)
 
 tk.mainloop()
