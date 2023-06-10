5c1e077a1af74cc89b5693eea32fc9429ed6e267
viktoriussuwandi <viktorius.tori@gmail.com>
2023-06-10 15:30:00 +0000

    update project sections

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ tk.config( padx = 20, pady = 20,bg = "#36486b")
 canvas = Canvas(master = tk, width = canvas_width, height = canvas_height)
 
 canvas.config(bg = "#fefbd8", highlightthickness = 0)
+
 canvas.grid( 
   padx   = canvas_pad_x, 
   pady   = canvas_pad_y, 
@@ -38,85 +39,48 @@ canvas.grid(
   columnspan = 4
 )
 
-
 #-----------------------------------------------------
-# CANVAS IMAGE
+# IMAGES
 #-----------------------------------------------------
+bg1_img = ImageTk.PhotoImage(Image.open("files/img/background1.png").resize(canvas_img_size))
+bg2_img = ImageTk.PhotoImage(Image.open("files/img/background2.png").resize(canvas_img_size))
+bg3_img = ImageTk.PhotoImage(Image.open("files/img/background3.png").resize(canvas_img_size))
 
-def set_img( path = None ) :
-  open_image   = Image.open(path)
-  resize_img   = open_image.resize(canvas_img_size)
-  result_image = ImageTk.PhotoImage(resize_img)
-  return result_image
-  
-beginning_img = set_img("files/img/background1.png")
-canvas_img = canvas.create_image(canvas_img_pos, image=beginning_img)
+btn_upload_img   = ImageTk.PhotoImage(Image.open("files/img/btn_upload_img.png").resize(btn_size))
+btn_download_img = ImageTk.PhotoImage(Image.open("files/img/btn_download_img.png").resize(btn_size))
+btn_add_text_img = ImageTk.PhotoImage(Image.open("files/img/btn_add_text_img.png").resize(btn_size))
 
-#-----------------------------------------------------
-# BUTTON UPLOAD
-#-----------------------------------------------------
-
-def upload_img() :
-  img_path = "files/img/background2.png"
-  img = PhotoImage(file=img_path)
-  print(img)
-  canvas.itemconfig(canvas_img, image=img)
-  print('Upload Image')
-
-logo_btn_upload = Image.open("files/img/btn_upload_img.png")
-logo_btn_upload_img = logo_btn_upload.resize(btn_size)
-btn_upload_img = ImageTk.PhotoImage(logo_btn_upload_img)
-btn_upload = Button(
-  image = btn_upload_img, 
-  highlightthickness = 0, 
-  command = upload_img
-)
-
-btn_upload.grid(pady = padding_btn_y, row=1, column=0)
-
-#-----------------------------------------------------
-# BUTTON DOWNLOAD
-#-----------------------------------------------------
-
-def download_img() :
-  img_path = "files/img/background3.png"
-  img = PhotoImage(file=img_path)
-  # img = set_img(img_path)
-  print(img)
-  canvas.itemconfig(canvas_img, image=img)
-  print('Download Image')
-  
-logo_btn_download = Image.open("files/img/btn_download_img.png")
-logo_btn_download_img = logo_btn_download.resize(btn_size)
-btn_download_img = ImageTk.PhotoImage(logo_btn_download_img)
-btn_donwload = Button(
-  image = btn_download_img,
-  highlightthickness = 0, 
-  command = download_img
-)
-
-btn_donwload.grid(pady = padding_btn_y, row=1, column=1)
-
-#-----------------------------------------------------
-# BUTTON ADD TEXT
-#-----------------------------------------------------
-
-def add_text() :
-  print('Add Text')
-  
-logo_btn_add_text = Image.open("files/img/btn_add_text_img.png")
-logo_btn_add_text_img = logo_btn_add_text.resize(btn_size)
-btn_add_text_img = ImageTk.PhotoImage(logo_btn_add_text_img)
-btn_add_text = Button(
-  image = btn_add_text_img,
-  highlightthickness = 0, 
-  command = add_text
-)
-
-btn_add_text.grid(pady = padding_btn_y, row=1, column=2)
+canvas_img_container = canvas.create_image( canvas_img_pos, image = bg1_img)
 
 #-----------------------------------------------------
 # FUNCTIONS
+#-----------------------------------------------------
+
+def upload_img() :
+  canvas.itemconfig(canvas_img_container, image = bg2_img)
+  print('Upload Image')
+
+def download_img() :
+  canvas.itemconfig(canvas_img_container, image = bg3_img)
+  print('Download Image')
+  
+def add_text() :
+  print('Add Text')
+
+#-----------------------------------------------------
+# ELEMENTS
+#-----------------------------------------------------
+btn_upload = Button( image = btn_upload_img, highlightthickness = 0, command = upload_img)
+btn_upload.grid(pady = padding_btn_y, row=1, column=0)
+
+btn_donwload = Button( image = btn_download_img, highlightthickness = 0, command = download_img )
+btn_donwload.grid(pady = padding_btn_y, row=1, column=1)
+
+btn_add_text = Button( image = btn_add_text_img, highlightthickness = 0, command = add_text )
+btn_add_text.grid(pady = padding_btn_y, row=1, column=2)
+
+#-----------------------------------------------------
+# OTHER
 #-----------------------------------------------------
 
 tk.mainloop()
