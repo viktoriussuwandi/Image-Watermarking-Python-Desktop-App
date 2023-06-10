0e053678fef1ee4976433dd1e4196526491e07a5
viktoriussuwandi <viktorius.tori@gmail.com>
2023-06-10 06:18:45 +0000

    re-structure the project

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-import sys
-sys.path.append("files/controller")
+# import sys
+# from PIL import Image,ImageTk
 
-from interface import Interface
+from tkinter import *
 
-ui = Interface()
+tk = Tk()
+tk.title("Watermark Desktop App")
+tk.geometry("500x600")
+# tk.minsize(width=500,height=600)
+tk.config(bg="#fefbd8")
+
+canvas = Canvas(width=450, height=350)
+canvas.config(bg="#B1DDC6", highlightthickness=0)
+canvas.grid(row=0, column=0)
+canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+bg_pos = (0, 0)
+bg_front = PhotoImage(file="files/img/background.png")
+background = canvas.create_image(bg_pos, image=bg_front)
+canvas.itemconfig(background, image=bg_front)
+
+
+tk.mainloop()
